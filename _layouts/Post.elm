45fab20a1434542f88acc496fb1f9963f7ba0b64
Orasund <orasund@gmail.com>
2022-10-06 11:41:53 +0200

    make table of contents sticky

--- _layouts/Post.elm
+++ _layouts/Post.elm
@@ -39,5 +39,5 @@ main =
                             ++ [ Html.hr [] [], Page.markdown blocks ]
                    )
             )
-                |> Page.layout content.title (content.content|> Page.parseBlocks|>Page.tableOfContent )
+                |> Page.layout content.title (content.content|> Page.parseBlocks|>Page.tableOfContent|> Layout.el [Attr.position "sticky", Attr.top "0"] )
                 |> Ok
