3cb7754faa24da65c3b7a1b245e6c811069d5fd7
Orasund <orasund@gmail.com>
2022-10-18 20:31:44 +0200

    fix

--- _layouts/Post.elm
+++ _layouts/Post.elm
@@ -55,8 +55,8 @@ main =
                 blocks =
                     content.content
                         ++ "\n"
-                        ++ "---"
-                        ++ "Did you like this post or want to share some thought? Then please write me a message on [Elm Discourse](https://discourse.elm-lang.org/u/lucas_payr)."
+                        ++ "---\n"
+                        ++ "Did you like this post or want to share some thoughts? Then ping me on [Elm Discourse](https://discourse.elm-lang.org/u/lucas_payr)."
                         |> Page.parseBlocks
             in
             [ metadataHtml content
