394831cbbf9a94a168960a288bd87cecd34a24b0
Orasund <orasund@gmail.com>
2022-10-20 04:47:12 +0200

    add critic to clean code

--- _layouts/Styles.elm
+++ _layouts/Styles.elm
@@ -37,7 +37,7 @@ styles =
             , Css.px -8 |> Css.marginRight
             ]
         , class "title"
-            [ fontSize <| Css.em 1.8
+            [ fontSize <| Css.em 32
             , Css.property "margin-block-start" "0px"
             , Css.property "margin-block-end" "64px"
             , Css.height Css.inherit
@@ -47,7 +47,7 @@ styles =
             , Css.px -24 |> Css.marginRight
             ]
         , h1
-            [ fontSize <| Css.em 1.33333
+            [ fontSize <| Css.em 24
             , Css.borderBottom3 (Css.px 1) Css.solid (Css.hex "363636")
             , Css.property "margin-block-start" "64px"
             , Css.property "margin-block-end" "32px"
@@ -55,7 +55,7 @@ styles =
             , Css.px 32 |> Css.height
             ]
         , h2
-            [ fontSize <| Css.em 1.2
+            [ fontSize <| Css.px 22
             , marginBottom <| rem 0.80808
             , Css.property "margin-block-start" "32px"
             , Css.property "margin-block-end" "32px"
